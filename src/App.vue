c2a015374ce51ac451119552279d1cdb96bf7b86
Moer <2655795601@qq.com>
2020-10-27 17:12:24 +0800

    home页面 下 一对一课程 页面

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <router-view></router-view>
 
+    <router-view></router-view>
+    
   </div>
 </template>
 
 <script>
-
 export default {
   name: "app",
-  components: { },
-  data() {
-    return {};
-  }
+  components: {}
 };
 </script>
 
@@ -24,9 +21,12 @@ body,
   height: 100%;
   list-style: none;
   font-size: 0.16rem;
+  background-color: #F0F2F5;
 }
 * {
   margin: 0;
   padding: 0;
 }
+
+  
 </style>

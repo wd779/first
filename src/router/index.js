96403bed2cb2ecce2d3293d1225cdf0778513d7c
HuangRenJie0808 <m18843108067@163.com>
2020-10-27 16:58:44 +0800

    路由提交

--- src/router/index.js
+++ src/router/index.js
@@ -6,21 +6,21 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/', name: 'Dome', component: () => import('../views/Dome.vue'),redirect: '/home',  children: [
+    path: '/',
+    name: 'Dome',
+    component: () => import('../views/Dome.vue'),
+    redirect: '/home',
+    children: [ // 二级路由
       { path: 'home', name: 'Home', component: Home },//首页
       { path: 'mine', name: 'mine', component: () => import('../views/mine.vue') }, // 我的
       { path: 'Appointment', name: 'Appointment', component: () => import('../views/Appointment.vue') }, // 课程
-      {
-        path: "practice", name: "Practice", component: () => import("../views/practice/Practice.vue"),
-        children: [
-          { path: "point", name: "point", component: () => import("../views/practice/point.vue") }, //考点练习
-          { path: "paper_package", name: "paper_package", component: () => import("../views/practice/paper_package.vue") }, //套卷联系
-          { path: "exam", name: "exam", component: () => import("../views/practice/exam.vue") },  // 仿真模考
-          { path: "error_ques", name: "error_ques", component: () => import("../views/practice/error_ques.vue") },// 错题练习
-          { path: "assess", name: "assess", component: () => import("../views/practice/assess.vue") }, // 测评记录
-          { path: "ques_collect", name: "ques_collect", component: () => import("../views/practice/ques_collect.vue") }// 习题收藏
-        ]
-      },//   练习页面
+      { path: "practice", name: "Practice", component: () => import("../views/practice/Practice.vue"),},//   练习页面
+      { path: "point", name: "point", component: () => import("../views/practice/point.vue") }, //考点练习
+      { path: "paper_package", name: "paper_package", component: () => import("../views/practice/paper_package.vue") }, //套卷联系
+      { path: "exam", name: "exam", component: () => import("../views/practice/exam.vue") },  // 仿真模考
+      { path: "error_ques", name: "error_ques", component: () => import("../views/practice/error_ques.vue") },// 错题练习
+      { path: "assess", name: "assess", component: () => import("../views/practice/assess.vue") }, // 测评记录
+      { path: "ques_collect", name: "ques_collect", component: () => import("../views/practice/ques_collect.vue") }// 习题收藏
     ]
   }, // 主页面
   { path: '/login', name: 'Login', component: () => import('../views/login/Login.vue') }, // 密码登录
